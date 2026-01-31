9485b8b36eae0b6cc3ebb77b7c763eccb06167d4
A-CheshireCat <123159015+A-CheshireCat@users.noreply.github.com>
2026-01-31 10:58:10 +0200

    Mask1 - double jump - basic implementation

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,20 +1,42 @@
 extends CharacterBody3D
 
+const SPEED = 7.0
+const JUMP_VELOCITY = 9.5
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+var mask1_active: bool = true
+var mask2_active: bool = false
+var mask3_active: bool = false
 
+var double_jump_active: bool = false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	elif mask1_active:
+		double_jump_active = true
+		
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	# Handle double jump
+	if Input.is_action_just_pressed("jump") and double_jump_active and not is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		double_jump_active = false
 
 	# Handle mask switch
+	if Input.is_action_just_pressed("mask1"):
+		mask1_active = true
+		mask2_active = false
+		mask3_active = false
+	if Input.is_action_just_pressed("mask2"):
+		mask1_active = false
+		mask2_active = true
+		mask3_active = false
+	if Input.is_action_just_pressed("mask3"):
+		mask1_active = false
+		mask2_active = false
+		mask3_active = true
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -22,9 +44,7 @@ func _physics_process(delta: float) -> void:
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
-		#velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		#velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
