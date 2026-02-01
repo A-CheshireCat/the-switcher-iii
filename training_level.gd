481d8898ebd14c0f877821e295bb544e17edb52e
OdiMeister <145771516+OdiMeister@users.noreply.github.com>
2026-02-01 18:18:05 +0200

    level2

--- training_level.gd
+++ training_level.gd
@@ -15,7 +15,7 @@ func _on_button_button_down() -> void:
 	get_tree().reload_current_scene()
 
 func _on_area_3d_area_entered(_area: Area3D) -> void:
-	get_tree().change_scene_to_file("res://win_screen.tscn")
+	get_tree().change_scene_to_file("res://second_level.tscn")
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
