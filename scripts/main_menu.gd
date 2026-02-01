d5f6fa98f0121f63e63b5c4e5e0e61cc3a3eb663
OdiMeister <145771516+OdiMeister@users.noreply.github.com>
2026-02-01 10:27:11 +0200

    Revert "Merge branch 'main' of https://github.com/A-CheshireCat/the-switcher-iii"
    This reverts commit c239c612d745753e45ffa814b577428e4a724026, reversing
    changes made to 9deb3826d57c08d43f399f07492df862508fb793.

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
-@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	audio_stream_player.play()
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
